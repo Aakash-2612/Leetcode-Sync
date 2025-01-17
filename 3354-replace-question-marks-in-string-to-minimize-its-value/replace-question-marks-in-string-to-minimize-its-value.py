e635ae6b2b110fdfd1b506d3bbd4261857a786b8
Aakash Kumar Yadav <113634738+Aakash-2612@users.noreply.github.com>
2025-01-17 20:59:46 +0530

    Time: 382 ms (52.89%) | Memory: 20.1 MB (26.92%) - LeetSync

---
+++ 3354-replace-question-marks-in-string-to-minimize-its-value/replace-question-marks-in-string-to-minimize-its-value.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minimizeStringValue(self, s: str) -> str:
+        d = {chr(key): -1 for key in range(97, 123)}
+        for i in s:
+            if i.isalpha():
+                d[i] += 1
+        min_heap = [[value, key] for key, value in d.items()]
+        heapq.heapify(min_heap)
+        # print(min_heap)
+
+        ans = []
+        res = ''
+        for i in s:
+            if i == '?':
+                cur = heapq.heappop(min_heap)
+                ans.append(cur[1])
+                cur[0] += 1
+                heapq.heappush(min_heap, cur)
+        
+        ind = 0
+        ans.sort()
+        print(ans)
+        for i in s:
+            if i == '?':
+                res += ans[ind]
+                ind += 1
+            else:
+                res += i
+        return res
+
+
