278bc5edc959c33edb7ae8d2752909a5f3248aa7
Aakash Kumar Yadav <113634738+Aakash-2612@users.noreply.github.com>
2025-01-18 22:14:37 +0530

    Time: 20 ms (8.71%) | Memory: 18.3 MB (7.20%) - LeetSync

---
+++ 1994-minimum-number-of-swaps-to-make-the-binary-string-alternating/minimum-number-of-swaps-to-make-the-binary-string-alternating.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        d1 = {'0': 0, '1': 0}
+        d2 = {'0': 0, '1': 0}
+        temp = 1
+        for i in s:
+            print(i, int(temp))
+            if int(i) != int(temp):
+                d1[i] += 1
+            else:
+                d2[i] += 1
+            temp = not temp
+
+        # print(d1)
+        # print(d2)
+        if d1['1'] != d1['0'] and d2['0'] != d2['1']:
+            return -1
+        else:   
+            if d1['1'] == d1['0']:
+                ans1 = d1['1']
+                if d2['1'] == d2['0']:
+                    return min(ans1, d2['1'])
+                else:
+                    return ans1
+            else:
+                return d2['1']
