40aebf08655ec6bab9980995900b106f42ca49ef
Aakash Kumar Yadav <113634738+Aakash-2612@users.noreply.github.com>
2025-02-06 11:50:57 +0530

    Time: 7 ms (81.12%) | Memory: 23.7 MB (5.38%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,4 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        myStr = ''.join(re.findall(r'[a-zA-Z0-9]+', s)).lower()
-        return myStr[::-1] == myStr
+        arr = []
+        for i in s:
+            if i.isalnum():
+                arr.append(i.lower())
+        
+        return arr[::-1] == arr
