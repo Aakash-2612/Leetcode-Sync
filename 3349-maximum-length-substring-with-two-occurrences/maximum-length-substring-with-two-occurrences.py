5ad4c90c4d833fb8c01ecafd8286eec7fe707526
Aakash Kumar Yadav <113634738+Aakash-2612@users.noreply.github.com>
2025-02-08 19:27:54 +0530

    Time: 96 ms (7.45%) | Memory: 17.7 MB (48.94%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.py
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.py
@@ -2,11 +2,12 @@ class Solution:
     def maximumLengthSubstring(self, s: str) -> int:
         ans = 0
         for i in range(len(s)):
-            arr = [0 for i in range(26)]
+            arr = [0 for a in range(26)]
             for j in range(i, len(s)):
                 arr[ord(s[j]) - ord("a")] += 1
                 if max(arr) > 2:
                     ans = max(ans, (j - i))
                     break
                 ans = max(ans, (j - i + 1))
+
         return ans
