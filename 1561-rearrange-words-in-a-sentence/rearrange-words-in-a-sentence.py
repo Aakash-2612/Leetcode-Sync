ea43af40cd6690603286aae3e967f9d4f9efce58
Aakash Kumar Yadav <113634738+Aakash-2612@users.noreply.github.com>
2025-02-27 21:52:02 +0530

    Time: 37 ms (5.39%) | Memory: 21.6 MB (6.06%) - LeetSync

--- 1561-rearrange-words-in-a-sentence/rearrange-words-in-a-sentence.py
+++ 1561-rearrange-words-in-a-sentence/rearrange-words-in-a-sentence.py
@@ -3,11 +3,10 @@ class Solution:
         min_heap = [[len(i), index, i.lower()] for index, i in enumerate(text.split())]
         # print(min_heap)
         heapq.heapify(min_heap)
-        ans = []
+        ans = ''
         while min_heap:
             cur = heapq.heappop(min_heap)
-            ans.append(cur[2])
+            ans += cur[2] + ' '
         
-        res = ' '.join(ans)
-        res = res[0].upper() + res[1:]
-        return res
+        ans = ans.rstrip()
+        return ans[0].upper() + ans[1:]
