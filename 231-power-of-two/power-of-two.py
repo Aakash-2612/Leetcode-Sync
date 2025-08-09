23dc33a15231b615fa1fffa343faf666f8d31c61
Aakash Kumar Yadav <113634738+Aakash-2612@users.noreply.github.com>
2025-08-09 11:18:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (80.74%) - LeetSync

---
+++ 231-power-of-two/power-of-two.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        else:
+            if n & n-1 != 0:
+                return False
+            else:
+                return True
