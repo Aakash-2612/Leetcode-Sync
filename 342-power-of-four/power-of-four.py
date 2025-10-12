5e9b683af8d2e32f0a74e56f93f7cdd240b3e535
Aakash Kumar Yadav <113634738+Aakash-2612@users.noreply.github.com>
2025-10-12 23:56:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (51.23%) - LeetSync

--- 342-power-of-four/power-of-four.py
+++ 342-power-of-four/power-of-four.py
@@ -1,19 +1,13 @@
+def sol(n):
+    if n < 1:
+        return False
+    if n == 1.0:
+        return True
+    if n%4 != 0:
+        return False
+        
+    return sol(n/4)
+
 class Solution(object):
     def isPowerOfFour(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
-        def sol(n):
-            if n < 1:
-                return False
-            
-            if n == 1:
-                return True
-
-            if n%4 != 0:
-                return False
-
-            return sol(n//4)
-
         return sol(n)
