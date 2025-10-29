ec6d4dd43ec03ac15ccdc518ae454d95889558d1
Aakash Kumar Yadav <113634738+Aakash-2612@users.noreply.github.com>
2025-10-30 01:50:38 +0530

    Time: 31 ms (15.84%) | Memory: 17.8 MB (41.79%) - LeetSync

--- 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.py
+++ 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.py
@@ -3,8 +3,9 @@ class Solution:
         ans = 0
         for i in range(len(arr)):
             for j in range(i, len(arr)):
-                if (j - i + 1) % 2 != 0:
+                if len(arr[i:j+1]) % 2 != 0:
                     # print(arr[i:j+1])
                     ans += sum(arr[i:j+1])
+
         
         return ans
